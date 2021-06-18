68104906d40a55e064d7cc2f0b03bc8f3ea69163
Otto <otcl@itu.dk>
2021-06-18 13:41:36 +0200

    SAT & LP example

--- pythonLib/testing.py
+++ pythonLib/testing.py
@@ -1,32 +1,29 @@
 from advalg.lp import *
 from advalg.cnf import CNF
-from advalg.dnf import DNF
 from advalg.sat_solver import *
 
-dnf = DNF(3)
-dnf.add_clause([1,-2,3])
-dnf.add_clause([-1,-3])
-dnf.add_clause([1])
-print(dnf)
-
+#SAT Example
 cnf = CNF(3)
 cnf.add_clause([1,-2,3])
 cnf.add_clause([-1,-3])
 cnf.add_clause([1])
-cnf.add_clause([-1])
-print(cnf)
+print(f"{cnf}\n")
 
 success, solution = solve(cnf)
 print(f"Satisfiable: {success}")
 if success:
     print(f"Solution: {solution}")
 
-# x = [Var(0), Var(1)]
-# lp = LP(x, Minimize(x[1] + x[0]))
-# lp.add_constraint(x[0] >= 1)
-# lp.add_constraint(x[1] >= 1)
-# lp.add_constraint(2*x[0] + 3*x[1] >= 10)
-# res = lp.solve()
+print()
 
-# print(f"Objective is: {res.obj_value}")
-# print(f"Variables: {res.variables}")
+#LP Example
+x = [Var(0), Var(1)]
+lp = LP(x, Minimize(x[1] + x[0]))
+lp.add_constraint(x[0] >= 1)
+lp.add_constraint(x[1] >= 1)
+lp.add_constraint(2*x[0] + 3*x[1] >= 10)
+print(f"{lp}\n")
+
+res = lp.solve()
+print(f"Objective is: {res.obj_value}")
+print(f"Variables: {res.variables}")
